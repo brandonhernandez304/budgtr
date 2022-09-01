4c84e6f6d41fdd061befb33086edfd0ddcd8cc51
Brandon Hernandez <bhernandez30497@gmail.com>
2022-08-31 21:06:37 -0400

    budget new completed

--- server.js
+++ server.js
@@ -8,27 +8,27 @@ const app = express();
 const port = 3000;
 
 //MIDWARE
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     console.log("Hi, I run for all routes")
     next()
 })
 //allows access to req.body
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 //allow access to public folder w express static
 app.use(express.static('public'))
 //MAIN ROUTE
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Budgtr")
 })
-/* I N D U C E S */ 
+/* I N D U C E S */
 //INDEX
-app.get("/budget/",(req,res)=>{
-    res.render("budget_index.ejs",{
+app.get("/budget/", (req, res) => {
+    res.render("budget_index.ejs", {
         allBudget: budget
     })
 });
 //NEW
-app.get("/budget/new",(req,res)=>{
+app.get("/budget/new", (req, res) => {
     res.render("budget_new.ejs")
 })
 //D
@@ -37,13 +37,13 @@ app.get("/budget/new",(req,res)=>{
 
 //E
 //SHOW
-app.get("/budget/:id", (req,res)=>{
-    res.render("budget_show.ejs",{
+app.get("/budget/:id", (req, res) => {
+    res.render("budget_show.ejs", {
         budgetItem: budget[req.params.id]
     })
 });
 
 //LISTEN 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('budgtr listening on port', port)
 });
